f6e5d8fa5b6da8da2b2d719b0814033557c54aa8
AzizAkturin <azizakturin@gmail.com>
2025-04-05 18:28:16 -0700

    analytics

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -46,6 +46,15 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="analytics"
+        options={{
+          title: "Analytics",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="chart.bar.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="settings"
         options={{
           title: "Settings",
